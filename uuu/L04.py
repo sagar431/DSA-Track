8fa5c8f705450ebd0f45553eac8432810b9fbbd6
sagar431 <sagarpallai1997@gmail.com>
2024-09-12 09:45:48 +0530

    Data type in python

---
+++ uuu/L04.py
@@ -0,0 +1,43 @@
+name="sagar"
+age=24
+gender='M'
+weight=60.17
+isTeacher=True
+
+
+
+
+
+
+fruitsIliked={"ornage","apple","banana"}
+
+
+earningFrom2022=[22,23,22,25,56]
+
+
+earningBefore2022=(22,23,24,22,29,30)
+
+
+#key values
+#Dictionary
+
+property={
+    "orange":5,
+    "onion":"7kg",
+    "mango":"1kg"
+
+}
+
+
+print(name,type(name))
+print(age,type(age))
+print(gender,type(gender))
+print(weight,type(weight))
+print(isTeacher,type(isTeacher))
+print(fruitsIliked,type(fruitsIliked ))
+print(earningFrom2022,type(earningFrom2022))
+
+
+
+
+
