108fef32e4ed97392db8a947daf75c58399dfa2c
sagar431 <sagarpallai1997@gmail.com>
2024-09-12 09:24:32 +0530

    Taking input in python

---
+++ uuu/L03.py
@@ -0,0 +1,10 @@
+name=input()
+age=int(input())
+salary=float(input())
+isAdmin=bool(input())
+
+
+print(name,type(name))
+print(age,type(age))
+print(salary,type(salary))
+print(isAdmin(type(isAdmin)))
