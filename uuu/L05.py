ba160ca960fe96b29c1024a5632df4a7445f5c92
sagar431 <sagarpallai1997@gmail.com>
2024-09-12 09:54:46 +0530

    List operation

---
+++ uuu/L05.py
@@ -0,0 +1,24 @@
+# firstname="sagar"
+# seccondName="pallai"
+
+# name=firstname+" "+seccondName
+# print(name)
+
+
+lst=[1,2,15,20]
+
+lst.append(25)
+print(lst)
+
+
+lst.insert(0,5)
+print(lst)
+
+
+lst.pop(-1)
+lst.pop(0)
+
+print(lst)
+
+lst[0]=21
+print(lst)
