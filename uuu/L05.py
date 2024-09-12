4030b3b32044eb624d01e0914d30bfe449d753c8
sagar431 <sagarpallai1997@gmail.com>
2024-09-12 09:55:56 +0530

    tuple o[erations done

--- uuu/L05.py
+++ uuu/L05.py
@@ -5,20 +5,24 @@
 # print(name)
 
 
-lst=[1,2,15,20]
+# lst=[1,2,15,20]
 
-lst.append(25)
-print(lst)
-
-
-lst.insert(0,5)
-print(lst)
+# lst.append(25)
+# print(lst)
 
 
-lst.pop(-1)
-lst.pop(0)
+# lst.insert(0,5)
+# print(lst)
 
-print(lst)
 
-lst[0]=21
-print(lst)
+# lst.pop(-1)
+# lst.pop(0)
+
+# print(lst)
+
+# lst[0]=21
+# print(lst)
+#tuple
+num=(3,4,5,6,12)
+
+print(num[0])
